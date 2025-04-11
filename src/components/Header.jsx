0e5c59f8baa2d9a37a0da92a2465a3b0d35c4ee9
atif128873806 <atifmumtaz455@gmail.com>
2025-04-11 07:46:32 +0500

    changes update

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+function Header () {
+
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>atuf</li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
